21d144a550fc574249708c93113d282b397834f2
Mateo_maramara <165224810+mateoMaramara@users.noreply.github.com>
2025-04-11 14:13:31 -1000

    Photo added to landingPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,7 @@ const Home = () => (
         </Col>
 
         <Col xs={8} className="d-flex flex-column justify-content-center">
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
+          <h1>Welcome to ManoaConnect</h1>
         </Col>
       </Row>
     </Container>
