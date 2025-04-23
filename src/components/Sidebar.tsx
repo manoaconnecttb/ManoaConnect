abf503cc2de5f457fdea72c16fa6789431b0d993
AndreLMiller <79182039+AndreLMiller@users.noreply.github.com>
2025-04-22 18:39:46 -1000

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,40 +4,71 @@ import React from 'react';
 import Image from 'next/image';
 import { HouseFill, FilePersonFill, Search } from 'react-bootstrap-icons';
 
-const Sidebar = () => (
-  <aside className="sidebar">
-    <div className="sidebar-header">
-      <Image
-        src="/ManoaConnectLogo.png"
-        alt="Logo"
-        className="sidebar-logo"
-        width={40}
-        height={40}
-        style={{ borderRadius: '50%' }}
-      />
-      <h2>Main Menu</h2>
-    </div>
-    <ul className="sidebar-links">
-      <li>
-        <a href="/">
-          <span><HouseFill /></span>
-          Home
-        </a>
-      </li>
-      <li>
-        <a href="/">
-          <span><FilePersonFill /></span>
-          Following
-        </a>
-      </li>
-      <li>
-        <a href="/Explore">
-          <span><Search /></span>
-          Explore
-        </a>
-      </li>
-    </ul>
-  </aside>
-);
+const Sidebar = () => {
+  const { data: session } = useSession();
+  const currentUser = session?.user?.email;
+  const userWithRole = session?.user as { email: string; randomKey: string };
+  const role = userWithRole?.randomKey;
+  const pathName = usePathname();
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-header">
+        <Image
+          src="/ManoaConnectLogo.png"
+          alt="Logo"
+          className="sidebar-logo"
+          width={40}
+          height={40}
+          style={{ borderRadius: '50%' }}
+        />
+        <h2>Main Menu</h2>
+      </div>
+      <ul className="sidebar-links">
+        <li>
+          <Link href="/home">
+            <span><HouseFill /></span>
+            Home
+          </Link>
+        </li>
+        {currentUser && (
+          <>
+            <li>
+              <Link href="/following" className={pathName === '/following' ? 'active' : ''}>
+                <span><FilePersonFill /></span>
+                Following
+              </Link>
+            </li>
+            <li>
+              <Link href="/clubs" className={pathName === '/clubs' ? 'active' : ''}>
+                <span><BorderAll /></span>
+                Clubs
+              </Link>
+            </li>
+            <li>
+              <Link href="/feedback" className={pathName === '/feedback' ? 'active' : ''}>
+                <span><PencilSquare /></span>
+                Feedback
+              </Link>
+            </li>
 
-export default Sidebar;
+          </>
+        )}
+        <li>
+          <Link href="/Explore">
+            <span><Search /></span>
+            Explore
+          </Link>
+        </li>
+
+        {/* Admin Only Link */}
+        {currentUser && role === 'ADMIN' && (
+          <li>
+            <Link href="/admin" className={pathName === '/admin' ? 'active' : ''}>
+              Admin
+            </Link>
+          </li>
+        )}
+      </ul>
+    </aside>
+  );
+};
