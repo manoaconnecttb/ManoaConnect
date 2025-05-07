183eddceaa6ad7ee14eecb0bfd0fc8374d6c19ae
keithvillanueva <lkv27@hawaii.edu>
2025-05-06 20:46:30 -1000

    fix?

--- tests/explore-posts.spec.ts
+++ tests/explore-posts.spec.ts
@@ -5,7 +5,7 @@ test.use({
 });
 
 test('Explore Posts Page', async ({ page }) => {
-  await page.goto('http://localhost:3000/test');
+  await page.goto('http://127.0.0.1:3000/test');
   await expect(page.getByRole('heading', { name: 'Explore Clubs' })).toBeVisible();
   await expect(page.getByRole('heading', { name: 'Create a Club' })).toBeVisible();
 });
