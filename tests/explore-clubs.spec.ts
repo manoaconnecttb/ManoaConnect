d20869e8e0f879ecb66b9d2f217f449f21722cdb
keithvillanueva <lkv27@hawaii.edu>
2025-05-06 21:28:45 -1000

    Revert "Merge pull request #117 from manoaconnecttb/dev-Keith-89"
    This reverts commit 8a218da03926b7d080dbdb8a21112fb114380783, reversing
    changes made to 361ad2bf7824e55d59dca680cd3d629a16c6dec1.

--- tests/explore-clubs.spec.ts
+++ tests/explore-clubs.spec.ts
@@ -4,7 +4,7 @@ test.use({
   storageState: 'admin-auth.json',
 });
 
-test('Explore Clubs Page', async ({ page }) => {
+test('Admin Pages', async ({ page }) => {
   await page.goto('http://localhost:3000/Explore');
   await expect(page.getByRole('heading', { name: 'Explore Posts' })).toBeVisible();
   await expect(page.getByRole('button', { name: 'Create Post' })).toBeVisible();
--- tests/home-page.spec.ts
+++ tests/home-page.spec.ts
@@ -4,7 +4,7 @@ test.use({
   storageState: 'admin-auth.json',
 });
 
-test('Home Page', async ({ page }) => {
+test('Admin Pages', async ({ page }) => {
   await page.goto('http://localhost:3000/home');
   await expect(page.getByRole('heading', { name: 'Posts' })).toBeVisible();
 });
